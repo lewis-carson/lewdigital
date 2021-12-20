892811c279a6cfaf07a1b3fcdc06d113abb26dba
Lewis Carson <lcarson679@wallacehigh.net>
2021-12-20 17:43:08 +0000

    tracked

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,127 @@
 import Head from 'next/head'
-import { motion } from 'framer-motion'
+
+import { motion, AnimatePresence, useAnimation } from 'framer-motion'
+import { useState } from 'react'
+import Link from 'next/link'
+
+const delay = 5
+
+function Nametag({ onEnd }) {
+  return <motion.div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+    <motion.div
+      className='text-4xl lg:text-8xl font-display'
+      initial={{
+        opacity: 1,
+      }}
+      animate={{
+        opacity: 1,
+      }}
+      exit={{
+        opacity: 0,
+      }}
+
+      transition={{
+        type: 'easeInOut',
+        duration: 1
+      }}
+    >
+      <motion.span
+        initial={{
+          left: "0%"
+        }}
+        animate={{
+          left: "33.333%"
+        }}
+
+        transition={{
+          type: 'easeInOut',
+          duration: 1,
+          delay: delay
+        }}
+
+        onAnimationComplete={onEnd}
+
+        className='relative'
+      >
+        lew
+      </motion.span>
+      <motion.span
+        initial={{
+          left: "0%",
+          opacity: 1
+        }}
+        animate={{
+          opacity: 0,
+          left: "33.333%"
+        }}
+
+        transition={{
+          type: 'easeInOut',
+          duration: 1,
+          delay: delay
+        }}
+
+        className='relative'
+      >.digital</motion.span>
+    </motion.div>
+  </motion.div >
+}
+
+function A({ href, children }) {
+  return <Link href={href}>
+    <span className="text-blue-500 hover:text-blue-300 transition-all cursor-pointer">
+      {children} ↗
+    </span>
+  </Link>
+}
+
+function Details() {
+  return <motion.div
+    className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'
+
+    initial={{
+      opacity: 0,
+      display: "block"
+    }}
+    animate={{
+      opacity: 1
+    }}
+
+    transition={{
+      type: 'easeInOut',
+      duration: 1,
+      delay: 1
+    }}
+    key="details"
+  >
+    <div className='text-lg'>
+      <i>2022 bruh <br /> i promise you</i> <br />
+      <br />
+      <ul>
+        <li>
+          <A href="mailto:torvimmm@gmail.com">mail</A>
+
+        </li>
+        <li>
+          <A href="https://github.com/lew-d">github</A>
+        </li>
+        {/*
+        <li>
+          <A href="">blog</A>
+        </li>
+        */}
+      </ul>
+    </div>
+  </motion.div>
+}
 
 export default function Home() {
+  var [isOpen, setIsOpen] = useState(false)
+
+  var onEnd = () => {
+    setIsOpen(true)
+  }
+
   return (
     <div className="">
       <Head>
@@ -9,22 +129,14 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <span className="absolute m-32 text-lg font-[FKS]">
-          Do you remember the first time?
-          <div className="h-10"></div>
-          <span className="inline-block w-40"></span> I remember <span className="inline-block w-40"></span>
-          <span className="text-3xl">the first time</span>
-        </span>
-
-        <div className="absolute m-56 mt-72 font-[FK] font-black tracking-[-0.1rem]">
-          <span className="text-[15vw]">
-            lew
-          </span>
-          <span className="m-32 pt-10 absolute text-[7vw]">
-            .digital
-          </span>
-        </div>
+      <main className='font-body'>
+        <AnimatePresence>
+          {
+            !isOpen ?
+              <Nametag onEnd={onEnd} key="nametag" /> :
+              <Details key="details" />
+          }
+        </AnimatePresence>
       </main>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily: {
+      "body": "Inter",
+      "display": "FK"
+    },
     extend: {},
   },
   plugins: [],

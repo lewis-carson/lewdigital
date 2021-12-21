fd5bd51e4a4ef92b7a6a62452e3f37023b59b1f6
Lewis Carson <lcarson679@wallacehigh.net>
2021-12-21 18:02:24 +0000

    ready4it

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,54 +1,18 @@
 import fs from 'fs'
 import matter from 'gray-matter'
-import { MDXRemote } from 'next-mdx-remote'
 import { serialize } from 'next-mdx-remote/serialize'
-//import dynamic from 'next/dynamic'
-import Head from 'next/head'
-import Link from 'next/link'
 import path from 'path'
-import CustomLink from '../../components/CustomLink'
 import Layout from '../../components/Layout'
 import { postFilePaths, POSTS_PATH } from '../../utils/mdxUtils'
 import readingTime from 'reading-time'
+import Post from '../../components/Post'
 
-const components = {
-    a: CustomLink,
-    strong: ({ children }) =>
-        <span className='text-2xl font-display font-bold'>{children}</span>,
-    pre: ({ children }) =>
-        <pre className='border p-5 rounded my-5'>{children}</pre>,
-    // It also works with dynamically-imported components, which is especially
-    // useful for conditionally loading components for certain routes.
-    // See the notes in README.md for more details.
-    //TestComponent: dynamic(() => import('../../components/TestComponent')),
-    Head,
-}
+import numWords from 'num-words'
 
-function A({ href, children }) {
-    return <Link href={href}>
-        <span className="text-blue-500 hover:text-blue-300 text-3xl font-black transition-all cursor-pointer">
-            {children}
-        </span>
-    </Link>
-}
-
-
-export default function PostPage({ source, frontMatter, timeToRead }) {
+export default function PostPage(props) {
     return (
         <Layout>
-            <div className='w-[70vw] lg:w-[40vw]'>
-                <A href="/blog">{"<---"}</A>
-                <div className='my-10'>
-                    <h1 className='font-display text-5xl'>{frontMatter.title}</h1>
-                    <span>{timeToRead}</span>
-                    {frontMatter.description && (
-                        <p className="description">{frontMatter.description}</p>
-                    )}
-                </div>
-                <main className=' text-justify'>
-                    <MDXRemote {...source} components={components} />
-                </main>
-            </div>
+            <Post {...props} />
         </Layout>
     )
 }
@@ -68,11 +32,13 @@ export const getStaticProps = async ({ params }) => {
         scope: data,
     })
 
+    var length = Math.ceil(readingTime(content).minutes)
+
     return {
         props: {
             source: mdxSource,
             frontMatter: data,
-            timeToRead: readingTime(content).text
+            timeToRead: numWords(length) + " min approx. reading time"
         },
     }
 }

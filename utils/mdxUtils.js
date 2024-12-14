1078db7367c85bece7011f68e0d012847e9fe8d6
Lewis Carson <lcarson679@wallacehigh.net>
2024-12-14 21:31:42 +0000

    added post(s)

--- utils/mdxUtils.js
+++ utils/mdxUtils.js
@@ -7,6 +7,7 @@ export const POSTS_PATH = path.join(process.cwd(), 'posts')
 // postFilePaths is the list of all mdx files inside the POSTS_PATH directory
 export const postFilePaths = [
    // "map_plotting.mdx",
+   // "dash.mdx",
     "willow.mdx",
     "whatgrad.mdx",
     "chess.mdx",
